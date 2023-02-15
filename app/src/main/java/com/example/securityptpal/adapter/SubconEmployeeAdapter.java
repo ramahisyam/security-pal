5bf5c6bbf30ff81482604cf0cef3fa03386c7710
ramahisyam2 <ramahisyam1@gmail.com>
2023-02-15 15:08:29 +0700

    edit delete employee subcon

--- app/src/main/java/com/example/securityptpal/adapter/SubconEmployeeAdapter.java
+++ app/src/main/java/com/example/securityptpal/adapter/SubconEmployeeAdapter.java
@@ -19,18 +19,20 @@ public class SubconEmployeeAdapter extends RecyclerView.Adapter<SubconEmployeeAd
     private Context context;
     private List<EmployeeSubcon> list;
     private OnPermitListener mOnPermitListener;
+    private OnPermitLongClick mOnPermitLongClick;
 
-    public SubconEmployeeAdapter(Context context, List<EmployeeSubcon> list, OnPermitListener mOnPermitListener) {
+    public SubconEmployeeAdapter(Context context, List<EmployeeSubcon> list, OnPermitListener mOnPermitListener, OnPermitLongClick mOnPermitLongClick) {
         this.context = context;
         this.list = list;
         this.mOnPermitListener = mOnPermitListener;
+        this.mOnPermitLongClick = mOnPermitLongClick;
     }
 
     @NonNull
     @Override
     public SubconEmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_subcon_employee, parent, false);
-        return new SubconEmployeeAdapter.SubconEmployeeViewHolder(itemView, mOnPermitListener);
+        return new SubconEmployeeAdapter.SubconEmployeeViewHolder(itemView, mOnPermitListener, mOnPermitLongClick);
     }
 
     @Override
@@ -44,21 +46,35 @@ public class SubconEmployeeAdapter extends RecyclerView.Adapter<SubconEmployeeAd
         return list.size();
     }
 
-    public class SubconEmployeeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class SubconEmployeeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
         TextView name, nip;
         OnPermitListener onPermitListener;
-        public SubconEmployeeViewHolder(@NonNull View itemView, OnPermitListener onPermitListener) {
+        OnPermitLongClick onPermitLongClick;
+        public SubconEmployeeViewHolder(@NonNull View itemView, OnPermitListener onPermitListener, OnPermitLongClick onPermitLongClick) {
             super(itemView);
             name = itemView.findViewById(R.id.subcon_employee_name);
             nip = itemView.findViewById(R.id.subcon_employee_nip);
 
             this.onPermitListener = onPermitListener;
+            this.onPermitLongClick = onPermitLongClick;
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
             onPermitListener.onPermitClick(getAdapterPosition());
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            if (onPermitLongClick != null) {
+                int pos = getAdapterPosition();
+                if (pos != RecyclerView.NO_POSITION){
+                    onPermitLongClick.onLongCLickListener(pos);
+                }
+            }
+            return true;
         }
     }
 }
